7490f99f1ebc27bf5352a17ed711b22165418637
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 05:53:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,45 @@
 class Artist
-  attr_accessor :name
-  @@song_count = 0
+  attr_accessor :name, :songs
+
+  @@all = []
+
   def initialize(name)
     @name = name
-    @songs = [] #empty collection of songs
+    @songs = []
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
+  end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def print_songs
+    songs.each { |song| puts song.name }
+  end
+
+  def save
+    @@all << self
   end
 
   def songs
     @songs
   end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def self.song_count
-    @@song_count
-  end
-
-end 
+end

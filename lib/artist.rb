731ac970819a1ef9b9f8484cdf4427aa3c74ac91
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 05:56:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,15 +13,6 @@ class Artist
     @@all
   end
 
-  def self.create(name)
-    artist = self.new(name)
-    artist.name = name
-    artist
-  end
-
-  def self.find(name)
-    self.all.detect { |artist| artist.name == name }
-  end
 
   def self.find_or_create_by_name(name)
     self.find(name) || self.create(name)
